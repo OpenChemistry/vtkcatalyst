55d834fd073f50710ecc2849c8c3a80a505eb197
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2020-08-26 14:52:19 -0400

    cmake: allow overriding of install destination

--- cmake/catalyst-macros.cmake
+++ cmake/catalyst-macros.cmake
@@ -7,7 +7,9 @@ Mark a target as the Catalyst API implementation.
 ~~~{.cmake}
 catalyst_library(
   TARGET    <target>
-  [INSTALL_EXPORT_DEFAULT])
+  [LIBRARY_DESTINATION <destination>]
+  [INSTALL_EXPORT_DEFAULT]
+  )
 ~~~
 
 The `TARGET` identifies the target added using an `add_library` call.
@@ -15,15 +17,22 @@ This function ensures that properties on the target are set up
 appropriately so that the generated library is ABI compatible
 with the Catalyst stub implementation.
 
+`LIBRARY_DESTINATION` if specified, is used to determine the install destination.
+If not specified, no install rule is added.
+
 `INSTALL_EXPORT_DEFAULT` is useful in projects that include Catalyst as a
 sub-project (instead of using it externally) to build Catalyst implementations.
 For such projects, the Catalyst library is often built as a part of the project.
 Providing INSTALL_EXPORT_DEFAULT ensures that the Catalyst implementation is
 added as a part of the export set for the Catalyst targets so that
 `find_package(catalyst ... )` can work seamlessly.
+If `LIBRARY_DESTINATION` must be specified if `INSTALL_EXPORT_DEFAULT`
+specified.
+
+
 #]==]
 function(catalyst_library)
-  cmake_parse_arguments(PARSE_ARGV 0 arg "INSTALL_EXPORT_DEFAULT" "TARGET" "")
+  cmake_parse_arguments(PARSE_ARGV 0 arg "INSTALL_EXPORT_DEFAULT" "TARGET;LIBRARY_DESTINATION" "")
   if (arg_UNPARSED_ARGUMENT)
     message(FATAL_ERROR
       "Unknown argument(s) passed to 'catalyst_library': '${arg_UNPARSED_ARGUMENT}'")
@@ -32,6 +41,11 @@ function(catalyst_library)
     message(WARNING
       "Target not named 'catalyst'. find_package(catalyst .. ) may not work as "
       "expected.")
+  endif()
+  if (arg_INSTALL_EXPORT_DEFAULT AND NOT DEFINED arg_LIBRARY_DESTINATION)
+    message(WARNING
+      "'INSTALL_EXPORT_DEFAULT' specified without 'LIBRARY_DESTINATION'."
+      "Argument has no effect.")
   endif()
 
   target_link_libraries(${arg_TARGET}
@@ -46,8 +60,16 @@ function(catalyst_library)
       VERSION   "${CATALYST_ABI_VERSION}"
       SOVERSION "${CATALYST_ABI_VERSION}"
       OUTPUT_NAME "catalyst")
+
+  set (_exports)
   if (arg_INSTALL_EXPORT_DEFAULT)
-    c_install_targets(${arg_TARGET})
+    set (_exports EXPORT Catalyst)
+  endif()
+  if (arg_LIBRARY_DESTINATION)
+    install(
+      TARGETS ${arg_TARGET}
+      ${_exports}
+      LIBRARY DESTINATION ${arg_LIBRARY_DESTINATION})
   endif()
 endfunction()
 
