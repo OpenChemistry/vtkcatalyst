b7e7bdd49aa5057ea03baccd23d099e889a4e86a
Tom Osika <tommycollege558@gmail.com>
2021-01-14 16:22:26 -0500

    Node parent tests work

--- tests/conduit_tests/t_conduit_node_parent.cpp
+++ tests/conduit_tests/t_conduit_node_parent.cpp
@@ -53,20 +53,20 @@
 #include "gtest/gtest.h"
 #include <iostream>
 
-using namespace conduit;
+using namespace conduit_cpp;
 
 //-----------------------------------------------------------------------------
 TEST(conduit_node_parent, simple)
 {
-  uint32 val1 = 10;
-  uint32 val2 = 20;
+  conduit_uint32 val1 = 10;
+  conduit_uint32 val2 = 20;
 
   Node n;
   n["a"]["b"] = val1;
   n["a"]["c"] = val2;
   EXPECT_EQ(n["a"]["b"].as_uint32(), val1);
   EXPECT_EQ(n["a"]["c"].as_uint32(), val2);
-  Node& b = n["a/c"];
+  Node b = n["a/c"];
   EXPECT_EQ(b["../b"].as_uint32(), val1);
   EXPECT_EQ(n["a/c/../b"].as_uint32(), val1);
 }
