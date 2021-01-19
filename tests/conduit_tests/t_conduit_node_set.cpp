b4d2f6796e26d283c325d85ae0d5ac0831a040be
Tom Osika <tommycollege558@gmail.com>
2021-01-19 18:06:19 -0500

    All set_external_bitwidth ptr tests pass

--- tests/conduit_tests/t_conduit_node_set.cpp
+++ tests/conduit_tests/t_conduit_node_set.cpp
@@ -622,69 +622,68 @@ TEST(conduit_node_set, set_path_bitwidth_uint_ptr)
   EXPECT_EQ(u64av_ptr[5], 64);
 }
 
-// //-----------------------------------------------------------------------------
-// TEST(conduit_node_set, set_external_bitwidth_uint_ptr)
-// {
-//     uint8    u8av[6] = {2,4,8,16,32,64};
-//     uint16  u16av[6] = {2,4,8,16,32,64};
-//     uint32  u32av[6] = {2,4,8,16,32,64};
-//     uint64  u64av[6] = {2,4,8,16,32,64};
+//-----------------------------------------------------------------------------
+TEST(conduit_node_set, set_external_bitwidth_uint_ptr)
+{
+  uint8 u8av[6] = { 2, 4, 8, 16, 32, 64 };
+  uint16 u16av[6] = { 2, 4, 8, 16, 32, 64 };
+  uint32 u32av[6] = { 2, 4, 8, 16, 32, 64 };
+  uint64 u64av[6] = { 2, 4, 8, 16, 32, 64 };
 
-//     Node n;
-//     // uint8
-//     n.set_external(u8av,6);
-//     uint8 *u8av_ptr = n.as_uint8_ptr();
-//     for(index_t i=0;i<6;i++)
-//     {
-//         EXPECT_EQ(u8av_ptr[i],u8av[i]);
-//          // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&u8av_ptr[i],&u8av[i]);
-//     }
-//     u8av_ptr[1] = 100;
-//     EXPECT_EQ(u8av[1],100);
-//     n.print();
+  Node n;
+  // uint8
+  n.set_external(u8av, 6);
+  uint8* u8av_ptr = n.as_uint8_ptr();
+  for (index_t i = 0; i < 6; i++)
+  {
+    EXPECT_EQ(u8av_ptr[i], u8av[i]);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&u8av_ptr[i], &u8av[i]);
+  }
+  u8av_ptr[1] = 100;
+  EXPECT_EQ(u8av[1], 100);
+  n.print();
 
-//     // uint16
-//     n.set_external(u16av,6);
-//     uint16 *u16av_ptr = n.as_uint16_ptr();
-//     for(index_t i=0;i<6;i++)
-//     {
-//         EXPECT_EQ(u16av_ptr[i],u16av[i]);
-//         // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&u16av_ptr[i],&u16av[i]);
-//     }
-//     u16av_ptr[1] = 100;
-//     EXPECT_EQ(u16av[1],100);
-//     n.print();
+  // uint16
+  n.set_external(u16av, 6);
+  uint16* u16av_ptr = n.as_uint16_ptr();
+  for (index_t i = 0; i < 6; i++)
+  {
+    EXPECT_EQ(u16av_ptr[i], u16av[i]);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&u16av_ptr[i], &u16av[i]);
+  }
+  u16av_ptr[1] = 100;
+  EXPECT_EQ(u16av[1], 100);
+  n.print();
 
-//     // uint32
-//     n.set_external(u32av,6);
-//     uint32 *u32av_ptr = n.as_uint32_ptr();
-//     for(index_t i=0;i<6;i++)
-//     {
-//         EXPECT_EQ(u32av_ptr[i],u32av[i]);
-//         // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&u32av_ptr[i],&u32av[i]);
-//     }
-//     u32av_ptr[1] = 100;
-//     EXPECT_EQ(u32av[1],100);
-//     n.print();
+  // uint32
+  n.set_external(u32av, 6);
+  uint32* u32av_ptr = n.as_uint32_ptr();
+  for (index_t i = 0; i < 6; i++)
+  {
+    EXPECT_EQ(u32av_ptr[i], u32av[i]);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&u32av_ptr[i], &u32av[i]);
+  }
+  u32av_ptr[1] = 100;
+  EXPECT_EQ(u32av[1], 100);
+  n.print();
 
-//     // uint64
-//     n.set_external(u64av,6);
-//     uint64 *u64av_ptr = n.as_uint64_ptr();
-//     for(index_t i=0;i<6;i++)
-//     {
-//         EXPECT_EQ(u64av_ptr[i],u64av[i]);
-//         // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&u64av_ptr[i],&u64av[i]);
-//     }
-//     EXPECT_EQ(u64av_ptr[5],64);
-//     u64av_ptr[1] = 100;
-//     EXPECT_EQ(u64av[1],100);
-//     n.print();
-
-// }
+  // uint64
+  n.set_external(u64av, 6);
+  uint64* u64av_ptr = n.as_uint64_ptr();
+  for (index_t i = 0; i < 6; i++)
+  {
+    EXPECT_EQ(u64av_ptr[i], u64av[i]);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&u64av_ptr[i], &u64av[i]);
+  }
+  EXPECT_EQ(u64av_ptr[5], 64);
+  u64av_ptr[1] = 100;
+  EXPECT_EQ(u64av[1], 100);
+  n.print();
+}
 
 // //-----------------------------------------------------------------------------
 // TEST(conduit_node_set, set_path_external_bitwidth_uint_ptr)
@@ -868,72 +867,71 @@ TEST(conduit_node_set_, set_path_bitwidth_int_ptr)
   EXPECT_EQ(i64av_ptr[5], -64);
 }
 
-// //-----------------------------------------------------------------------------
-// TEST(conduit_node_set_, set_external_bitwidth_int_ptr)
-// {
-//     int8    i8av[6] = {-2,-4,-8,-16,-32,-64};
-//     int16  i16av[6] = {-2,-4,-8,-16,-32,-64};
-//     int32  i32av[6] = {-2,-4,-8,-16,-32,-64};
-//     int64  i64av[6] = {-2,-4,-8,-16,-32,-64};
+//-----------------------------------------------------------------------------
+TEST(conduit_node_set_, set_external_bitwidth_int_ptr)
+{
+  int8 i8av[6] = { -2, -4, -8, -16, -32, -64 };
+  int16 i16av[6] = { -2, -4, -8, -16, -32, -64 };
+  int32 i32av[6] = { -2, -4, -8, -16, -32, -64 };
+  int64 i64av[6] = { -2, -4, -8, -16, -32, -64 };
 
-//     Node n;
-//     // int8
-//     n.set_external(i8av,6);
-//     int8 *i8av_ptr = n.as_int8_ptr();
-//     for(index_t i=0;i<6;i++)
-//     {
-//         EXPECT_EQ(i8av_ptr[i],i8av[i]);
-//        // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&i8av_ptr[i],&i8av[i]);
-//     }
-//     EXPECT_EQ(i8av_ptr[5],-64);
-//     i8av_ptr[1] = -100;
-//     EXPECT_EQ(i8av[1],-100);
-//     n.print();
+  Node n;
+  // int8
+  n.set_external(i8av, 6);
+  int8* i8av_ptr = n.as_int8_ptr();
+  for (index_t i = 0; i < 6; i++)
+  {
+    EXPECT_EQ(i8av_ptr[i], i8av[i]);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&i8av_ptr[i], &i8av[i]);
+  }
+  EXPECT_EQ(i8av_ptr[5], -64);
+  i8av_ptr[1] = -100;
+  EXPECT_EQ(i8av[1], -100);
+  n.print();
 
-//     // int16
-//     n.set_external(i16av,6);
-//     int16 *i16av_ptr = n.as_int16_ptr();
-//     for(index_t i=0;i<6;i++)
-//     {
-//         EXPECT_EQ(i16av_ptr[i],i16av[i]);
-//        // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&i16av_ptr[i],&i16av[i]);
-//     }
-//     EXPECT_EQ(i16av_ptr[5],-64);
-//     i16av_ptr[1] = -100;
-//     EXPECT_EQ(i16av[1],-100);
-//     n.print();
+  // int16
+  n.set_external(i16av, 6);
+  int16* i16av_ptr = n.as_int16_ptr();
+  for (index_t i = 0; i < 6; i++)
+  {
+    EXPECT_EQ(i16av_ptr[i], i16av[i]);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&i16av_ptr[i], &i16av[i]);
+  }
+  EXPECT_EQ(i16av_ptr[5], -64);
+  i16av_ptr[1] = -100;
+  EXPECT_EQ(i16av[1], -100);
+  n.print();
 
-//     // int32
-//     n.set_external(i32av,6);
-//     int32 *i32av_ptr = n.as_int32_ptr();
-//     for(index_t i=0;i<6;i++)
-//     {
-//         EXPECT_EQ(i32av_ptr[i],i32av[i]);
-//        // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&i32av_ptr[i],&i32av[i]);
-//     }
-//     EXPECT_EQ(i32av_ptr[5],-64);
-//     i32av_ptr[1] = -100;
-//     EXPECT_EQ(i32av[1],-100);
-//     n.print();
+  // int32
+  n.set_external(i32av, 6);
+  int32* i32av_ptr = n.as_int32_ptr();
+  for (index_t i = 0; i < 6; i++)
+  {
+    EXPECT_EQ(i32av_ptr[i], i32av[i]);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&i32av_ptr[i], &i32av[i]);
+  }
+  EXPECT_EQ(i32av_ptr[5], -64);
+  i32av_ptr[1] = -100;
+  EXPECT_EQ(i32av[1], -100);
+  n.print();
 
-//     // int64
-//     n.set_external(i64av,6);
-//     int64 *i64av_ptr = n.as_int64_ptr();
-//     for(index_t i=0;i<6;i++)
-//     {
-//         EXPECT_EQ(i64av_ptr[i],i64av[i]);
-//        // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&i64av_ptr[i],&i64av[i]);
-//     }
-//     EXPECT_EQ(i64av_ptr[5],-64);
-//     i64av_ptr[1] = -100;
-//     EXPECT_EQ(i64av[1],-100);
-//     n.print();
-
-// }
+  // int64
+  n.set_external(i64av, 6);
+  int64* i64av_ptr = n.as_int64_ptr();
+  for (index_t i = 0; i < 6; i++)
+  {
+    EXPECT_EQ(i64av_ptr[i], i64av[i]);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&i64av_ptr[i], &i64av[i]);
+  }
+  EXPECT_EQ(i64av_ptr[5], -64);
+  i64av_ptr[1] = -100;
+  EXPECT_EQ(i64av[1], -100);
+  n.print();
+}
 
 // //-----------------------------------------------------------------------------
 // TEST(conduit_node_set, set_path_external_bitwidth_int_ptr)
@@ -1074,81 +1072,81 @@ TEST(conduit_node_set, set_path_bitwidth_float_ptr)
   EXPECT_NEAR(f64av_ptr[3], -6.4, 0.001);
 }
 
-// //-----------------------------------------------------------------------------
-// TEST(conduit_node_set, set_external_bitwidth_float_ptr)
-// {
-//     float32  f32av[4] = {-0.8f, -1.6f, -3.2f, -6.4f};
-//     float64  f64av[4] = {-0.8, -1.6, -3.2, -6.4};
+//-----------------------------------------------------------------------------
+TEST(conduit_node_set, set_external_bitwidth_float_ptr)
+{
+  float32 f32av[4] = { -0.8f, -1.6f, -3.2f, -6.4f };
+  float64 f64av[4] = { -0.8, -1.6, -3.2, -6.4 };
 
-//     Node n;
-//     // float32
-//     n.set_external(f32av,4);
-//     float32 *f32av_ptr = n.as_float32_ptr();
-//     for(index_t i=0;i<4;i++)
-//     {
-//         EXPECT_NEAR(f32av_ptr[i],f32av[i],0.001);
-//         // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&f32av_ptr[i],&f32av[i]);
-//     }
-//     EXPECT_NEAR(f32av_ptr[3],-6.4,0.001);
-//     f32av_ptr[1] = -110.1f;
-//     EXPECT_NEAR(f32av[1],-110.1,0.001);
-//     n.print();
+  Node n;
+  // float32
+  n.set_external(f32av, 4);
+  float32* f32av_ptr = n.as_float32_ptr();
+  for (index_t i = 0; i < 4; i++)
+  {
+    EXPECT_NEAR(f32av_ptr[i], f32av[i], 0.001);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&f32av_ptr[i], &f32av[i]);
+  }
+  EXPECT_NEAR(f32av_ptr[3], -6.4, 0.001);
+  f32av_ptr[1] = -110.1f;
+  EXPECT_NEAR(f32av[1], -110.1, 0.001);
+  n.print();
 
-//     // float64
-//     n.set_external(f64av,4);
-//     float64 *f64av_ptr = n.as_float64_ptr();
-//     for(index_t i=0;i<4;i++)
-//     {
-//         EXPECT_NEAR(f64av_ptr[i],f64av[i],0.001);
-//         // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&f64av_ptr[i],&f64av[i]);
-//     }
-//     EXPECT_NEAR(f64av_ptr[3],-6.4,0.001);
-//     f64av_ptr[1] = -110.1;
-//     EXPECT_NEAR(f64av[1],-110.1,0.001);
-//     n.print();
-// }
+  // float64
+  n.set_external(f64av, 4);
+  float64* f64av_ptr = n.as_float64_ptr();
+  for (index_t i = 0; i < 4; i++)
+  {
+    EXPECT_NEAR(f64av_ptr[i], f64av[i], 0.001);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&f64av_ptr[i], &f64av[i]);
+  }
+  EXPECT_NEAR(f64av_ptr[3], -6.4, 0.001);
+  f64av_ptr[1] = -110.1;
+  EXPECT_NEAR(f64av[1], -110.1, 0.001);
+  n.print();
+}
 
-// //-----------------------------------------------------------------------------
-// TEST(conduit_node_set, set_path_external_bitwidth_float_ptr)
-// {
-//     float32  f32av[4] = {-0.8f, -1.6f, -3.2f, -6.4f};
-//     float64  f64av[4] = {-0.8, -1.6, -3.2, -6.4};
+//-----------------------------------------------------------------------------
+TEST(conduit_node_set, set_path_external_bitwidth_float_ptr)
+{
+  float32 f32av[4] = { -0.8f, -1.6f, -3.2f, -6.4f };
+  float64 f64av[4] = { -0.8, -1.6, -3.2, -6.4 };
 
-//     Node n;
-//     // float32
-//     n.set_path_external("two/lvl",f32av,4);
-//     EXPECT_TRUE(n.has_path("two"));
-//     EXPECT_TRUE(n["two"].has_path("lvl"));
-//     float32 *f32av_ptr = n["two/lvl"].as_float32_ptr();
-//     for(index_t i=0;i<4;i++)
-//     {
-//         EXPECT_NEAR(f32av_ptr[i],f32av[i],0.001);
-//         // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&f32av_ptr[i],&f32av[i]);
-//     }
-//     EXPECT_NEAR(f32av_ptr[3],-6.4,0.001);
-//     f32av_ptr[1] = -110.1f;
-//     EXPECT_NEAR(f32av[1],-110.1,0.001);
-//     n.print();
+  Node n;
+  // float32
+  n.set_path_external("two/lvl", f32av, 4);
+  EXPECT_TRUE(n.has_path("two"));
+  EXPECT_TRUE(n["two"].has_path("lvl"));
+  float32* f32av_ptr = n["two/lvl"].as_float32_ptr();
+  for (index_t i = 0; i < 4; i++)
+  {
+    EXPECT_NEAR(f32av_ptr[i], f32av[i], 0.001);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&f32av_ptr[i], &f32av[i]);
+  }
+  EXPECT_NEAR(f32av_ptr[3], -6.4, 0.001);
+  f32av_ptr[1] = -110.1f;
+  EXPECT_NEAR(f32av[1], -110.1, 0.001);
+  n.print();
 
-//     // float64
-//     n.set_path_external("two/lvl",f64av,4);
-//     EXPECT_TRUE(n.has_path("two"));
-//     EXPECT_TRUE(n["two"].has_path("lvl"));
-//     float64 *f64av_ptr = n["two/lvl"].as_float64_ptr();
-//     for(index_t i=0;i<4;i++)
-//     {
-//         EXPECT_NEAR(f64av_ptr[i],f64av[i],0.001);
-//         // set_external(...) semantics imply a ref -- mem addys should match
-//         EXPECT_EQ(&f64av_ptr[i],&f64av[i]);
-//     }
-//     EXPECT_NEAR(f64av_ptr[3],-6.4,0.001);
-//     f64av_ptr[1] = -110.1;
-//     EXPECT_NEAR(f64av[1],-110.1,0.001);
-//     n.print();
-// }
+  // float64
+  n.set_path_external("two/lvl", f64av, 4);
+  EXPECT_TRUE(n.has_path("two"));
+  EXPECT_TRUE(n["two"].has_path("lvl"));
+  float64* f64av_ptr = n["two/lvl"].as_float64_ptr();
+  for (index_t i = 0; i < 4; i++)
+  {
+    EXPECT_NEAR(f64av_ptr[i], f64av[i], 0.001);
+    // set_external(...) semantics imply a ref -- mem addys should match
+    EXPECT_EQ(&f64av_ptr[i], &f64av[i]);
+  }
+  EXPECT_NEAR(f64av_ptr[3], -6.4, 0.001);
+  f64av_ptr[1] = -110.1;
+  EXPECT_NEAR(f64av[1], -110.1, 0.001);
+  n.print();
+}
 
 // //-----------------------------------------------------------------------------
 // // C Style Checks
