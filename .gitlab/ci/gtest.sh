d89047e0be24d7658d93d40133ad4e963afa70af
Tom Osika <tom.osika@kitware.com>
2021-02-22 13:46:36 -0500

    gtest.sh: Add checksum

--- .gitlab/ci/gtest.sh
+++ .gitlab/ci/gtest.sh
@@ -3,16 +3,21 @@
 set -e
 
 readonly version="1.10.0"
-readonly TARBALL="release-$version.tar.gz"
-readonly TARGET="googletest-release-$version"
+readonly sha256sum="9dc9157a9a1551ec7a7e43daea9a694a0bb5fb8bec81235d8a1e6ef64c716dcb"
+readonly tarball="release-$version.tar.gz"
+readonly target="googletest-release-$version"
 
 cd .gitlab
 
-curl -OL "https://github.com/google/googletest/archive/$TARBALL"
-cmake -E tar xf "$TARBALL"
-cd "$TARGET"
+echo "$sha256sum  $tarball" > gtest.sha256sum
+curl -OL "https://github.com/google/googletest/archive/$tarball"
+sha256sum --check gtest.sha256sum
+
+cmake -E tar xf "$tarball"
+cd "$target"
 mkdir build
 cd build
+
 cmake .. -DBUILD_GMOCK=OFF -Dgtest_disable_pthreads=ON
 cmake --build .
 cmake --build . --target install
