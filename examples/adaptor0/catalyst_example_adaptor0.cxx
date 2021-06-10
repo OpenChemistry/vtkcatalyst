f88d0f301e2e700c41348e7327d081742c66537c
Ben Boeckel <ben.boeckel@kitware.com>
2021-06-10 18:17:12 -0400

    examples/adaptor0: update to use the new runtime loading API

--- examples/adaptor0/catalyst_example_adaptor0.cxx
+++ examples/adaptor0/catalyst_example_adaptor0.cxx
@@ -7,20 +7,33 @@
 #include <conduit.hpp>
 #include <conduit_cpp_to_c.hpp>
 
-//-----------------------------------------------------------------------------
-void catalyst_initialize(const conduit_node*) {}
+#include "catalyst_impl_example_adaptor0.h"
 
 //-----------------------------------------------------------------------------
-void catalyst_execute(const conduit_node*) {}
+enum catalyst_error catalyst_initialize_example_adaptor0(const conduit_node*)
+{
+  return catalyst_error_ok;
+}
 
 //-----------------------------------------------------------------------------
-void catalyst_finalize(const conduit_node*) {}
+enum catalyst_error catalyst_execute_example_adaptor0(const conduit_node*)
+{
+  return catalyst_error_ok;
+}
 
 //-----------------------------------------------------------------------------
-void catalyst_about(conduit_node* params)
+enum catalyst_error catalyst_finalize_example_adaptor0(const conduit_node*)
+{
+  return catalyst_error_ok;
+}
+
+//-----------------------------------------------------------------------------
+enum catalyst_error catalyst_about_example_adaptor0(conduit_node* params)
 {
   catalyst_stub_about(params);
   auto& cpp_params = (*conduit::cpp_node(params));
   cpp_params["catalyst"]["capabilities"].append().set("adaptor0");
   cpp_params["catalyst"]["implementation"] = "examples/adaptor0";
+
+  return catalyst_error_ok;
 }
