c2a6722c8e13da23fac822782602480ded40863d
Ben Boeckel <ben.boeckel@kitware.com>
2021-06-14 16:27:47 -0400

    ci: support macOS in cmake, ninja, and gtest download scripts

--- .gitlab/ci/gtest.sh
+++ .gitlab/ci/gtest.sh
@@ -7,11 +7,25 @@ readonly sha256sum="9dc9157a9a1551ec7a7e43daea9a694a0bb5fb8bec81235d8a1e6ef64c71
 readonly tarball="release-$version.tar.gz"
 readonly target="googletest-release-$version"
 
+case "$( uname -s )" in
+    Linux)
+        shatool="sha256sum"
+        ;;
+    Darwin)
+        shatool="shasum -a 256"
+        ;;
+    *)
+        echo "Unrecognized platform $( uname -s )"
+        exit 1
+        ;;
+esac
+readonly shatool
+
 cd .gitlab
 
 echo "$sha256sum  $tarball" > gtest.sha256sum
 curl -OL "https://github.com/google/googletest/archive/$tarball"
-sha256sum --check gtest.sha256sum
+$shatool --check gtest.sha256sum
 
 cmake -E tar xf "$tarball"
 cd "$target"
