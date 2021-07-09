11af2e185b8f02fe9cf4f3929e907e475ed6bf4f
Andrew Bauer <bauer.andrew.c@gmail.com>
2021-07-09 11:16:38 -0400

    Fix default_impl ordering of stub methods

--- src/catalyst/catalyst_api_default.cpp
+++ src/catalyst/catalyst_api_default.cpp
@@ -7,4 +7,4 @@
 #include "catalyst_stub.h"
 
 extern "C" struct catalyst_impl const default_impl = { 1, catalyst_stub_initialize,
-  catalyst_stub_finalize, catalyst_stub_execute, catalyst_stub_about, catalyst_stub_results };
+  catalyst_stub_execute, catalyst_stub_finalize, catalyst_stub_about, catalyst_stub_results };
