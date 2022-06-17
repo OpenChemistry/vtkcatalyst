2aeff59c7a592ddff1ebfabf82853f26f60c294c
Ben Boeckel <ben.boeckel@kitware.com>
2022-06-17 09:07:34 -0400

    gitlab-ci: fix release upload urls
    Generic packages require a `name/version` component; just a version is
    not sufficient.

--- .gitlab/ci/upload-files.sh
+++ .gitlab/ci/upload-files.sh
@@ -16,7 +16,7 @@ upload_file () {
 
     curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
         --upload-file "$file" \
-        "$urlbase/packages/generic/${CI_COMMIT_TAG}/$upload_file"
+        "$urlbase/packages/generic/catalyst/${CI_COMMIT_TAG}/$upload_file"
 }
 
 for file_to_upload in $UPLOAD_FILES; do
